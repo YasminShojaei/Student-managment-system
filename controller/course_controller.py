fba3a191ae2a35611e2c54d743e3fbc54addb527
YasminShojaei <yasminshojaeii@gmail.com>
2025-07-14 00:55:13 +0330

    controllers update

---
+++ controller/course_controller.py
@@ -0,0 +1,18 @@
+class CourseController:
+    def save_course(self):
+        pass
+
+    def edit_course(self):
+        pass
+
+    def delete_course(self):
+        pass
+
+    def find_by_id(self):
+        pass
+
+    def find_by_date(self):
+        pass
+
+    def find_by_name(self):
+        pass
---
+++ controller/student_controller.py
@@ -0,0 +1,16 @@
+class StudentController:
+    def save_student(self):
+        pass
+
+    def edit_student(self):
+        pass
+
+    def delete_student(self):
+        pass
+
+    def find_by_name(self):
+        pass
+
+    def find_by_id(self):
+        pass
+
