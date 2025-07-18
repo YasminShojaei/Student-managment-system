0ab3eb82ea480b8fc4dd010dc7b3eb234ed27473
YasminShojaei <yasminshojaeii@gmail.com>
2025-07-19 01:16:42 +0330

    controllers update

--- model/repository/database_creator.py
+++ model/repository/database_creator.py
@@ -1,13 +1,38 @@
 import sqlite3
 
 connection = sqlite3.connect("university_db.sqlite")
+cursor = connection.cursor()
 
-#
-# cursor = connection.cursor()
-#
-# cursor.execute("SQL")
-#
-# connection.commit()
-#
-# cursor.close()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students (
+    student_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name text NOT NULL,
+    family text NOT NULL,
+    birth_date text NOT NULL
+)
+""")
+
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS teachers (
+    teacher_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name text NOT NULL,
+    family text NOT NULL,
+    birth_date text NOT NULL
+)
+""")
+
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS courses (
+    course_id INTEGER primary key, 
+    course_teacher text NOT NULL,
+    title TEXT NOT NULL,
+    unit INTEGER NOT NULL,
+    course_date TEXT NOT NULL
+)
+""")
+
+connection.commit()
+cursor.close()
 connection.close()
--- model/repository/student_reposity.py
+++ model/repository/student_reposity.py
@@ -1,13 +1,58 @@
+import sqlite3
+from model.entity import student
+
+
 class StudentRepository:
     def save_student(self):
-        pass
+        connection = sqlite3.connect('university.db')
+        curses = connection.cursor()
+        curses.execute(
+            """insert into students
+                (student_id, name, family, birth_date)
+        values
+            (?, ?, ?, ?)""",
+            [student.student_id, student.name, student.family, student.birth_date]
+        )
 
-    def edit_student(self):
-        pass
+        connection.commit()
+        curses.close()
+        connection.close()
 
-    def delete_student(self):
-        pass
+    def edit_student(self, student):
+        connection = sqlite3.connect('university.db')
+        curses = connection.cursor()
+        curses.execute("update students set student_id=?, name= ?, family=?, birth_date=? where student_id = ?;", [student.student_id, student.name, student.family, student.birth_date])
+        connection.commit()
+        curses.close()
+        connection.close()
 
-    def find_by_id(self):
-        pass
+    def delete_student(self, student_id):
+        connection = sqlite3.connect('university.db')
+        curses = connection.cursor()
+        curses.execute("delete from students where student_id = ?", [student_id])
+        connection.commit()
+        connection.commit()
+        curses.close()
+        connection.close()
+
+    def find_all(self):
+        connection = sqlite3.connect('university.db')
+        curses = connection.cursor()
+        curses.execute("select * from students")
+        curses.close()
+        connection.close()
+
+    def find_by_id(self, student_id):
+        connection = sqlite3.connect('university.db')
+        curses = connection.cursor()
+        curses.execute("select * from students where student_id = ?;", [student_id])
+        curses.close()
+        connection.close()
+
+    def find_by_name_family(self, name, family):
+        connection = sqlite3.connect('university.db')
+        curses = connection.cursor()
+        curses.execute("select * from students where name like ? and family like ?;", [name + "%", family + "%"])
+        curses.close()
+        connection.close()
 
