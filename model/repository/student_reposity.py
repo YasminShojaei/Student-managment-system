ddb65e280de33258da5fad37ef1153ef2076480c
YasminShojaei <yasminshojaeii@gmail.com>
2025-07-19 22:14:08 +0330

    student repository update

--- model/repository/student_reposity.py
+++ model/repository/student_reposity.py
@@ -3,56 +3,56 @@ from model.entity import student
 
 
 class StudentRepository:
-    def save_student(self):
-        connection = sqlite3.connect('university.db')
-        curses = connection.cursor()
-        curses.execute(
-            """insert into students
-                (student_id, name, family, birth_date)
-        values
-            (?, ?, ?, ?)""",
-            [student.student_id, student.name, student.family, student.birth_date]
-        )
+    def connect(self):
+        self.connection = sqlite3.connect("university_db.sqlite")
+        self.cursor = self.connection.cursor()
 
-        connection.commit()
-        curses.close()
-        connection.close()
+    def disconnect(self, commit = False):
+        if commit:
+            self.connection.commit()
+        self.cursor.close()
+        self.connection.close()
+
+    def save_student(self, new_student):
+        self.connect()
+        self.cursor.execute(
+            """insert into students
+                   (student_id, name, family, birth_date)
+            values 
+                   (?, ?, ?, ?)""",
+            [new_student.student_id, new_student.name, new_student.family, new_student.birth_date]
+        )
+        self.disconnect(commit=True)
 
     def edit_student(self, student):
-        connection = sqlite3.connect('university.db')
-        curses = connection.cursor()
-        curses.execute("update students set student_id=?, name= ?, family=?, birth_date=? where student_id = ?;", [student.student_id, student.name, student.family, student.birth_date])
-        connection.commit()
-        curses.close()
-        connection.close()
+        self.connect()
+
+        self.cursor.execute("update students set student_id=?, name=?, family=?, birth_date=? where student_id = ?",
+                       [student.student_id, student.name, student.family, student.birth_date])
+        self.disconnect(commit = True)
 
     def delete_student(self, student_id):
-        connection = sqlite3.connect('university.db')
-        curses = connection.cursor()
-        curses.execute("delete from students where student_id = ?", [student_id])
-        connection.commit()
-        connection.commit()
-        curses.close()
-        connection.close()
+        self.connect()
+        self.cursor.execute("delete from students where student_id = ?", [student_id])
+        self.disconnect(commit = True)
 
     def find_all(self):
-        connection = sqlite3.connect('university.db')
-        curses = connection.cursor()
-        curses.execute("select * from students")
-        curses.close()
-        connection.close()
+        self.connect()
+        self.cursor.execute("select * from students")
+        student_list = self.cursor.fetchall()
+        self.disconnect()
+        return student_list
 
     def find_by_id(self, student_id):
-        connection = sqlite3.connect('university.db')
-        curses = connection.cursor()
-        curses.execute("select * from students where student_id = ?;", [student_id])
-        curses.close()
-        connection.close()
+        self.connect()
+        self.cursor.execute("select * from students where student_id = ?;", [student_id])
+        student_list = self.cursor.fetchone()
+        self.disconnect()
+        return student_list
 
     def find_by_name_family(self, name, family):
-        connection = sqlite3.connect('university.db')
-        curses = connection.cursor()
-        curses.execute("select * from students where name like ? and family like ?;", [name + "%", family + "%"])
-        curses.close()
-        connection.close()
-
+        self.connect()
+        self.cursor.execute("select * from students where name like ? and family like ?;", [name + "%", family + "%"])
+        student_list = self.cursor.fetchone()
+        self.disconnect()
+        return student_list
--- test/student_test.py
+++ test/student_test.py
@@ -1,5 +1,10 @@
 from model.entity.student import Student
+from model.repository.student_reposity import StudentRepository
 
-student_1 = Student(1, "yasmin", "Shojaei", "2002-02-22")
+
+student_1 = Student(4, "mahsa", "rezai", "2001-02-22")
 print(student_1.to_tuple())
 print(student_1.__repr__())
+
+student_repo = StudentRepository()
+student_repo.save_student(student_1)
